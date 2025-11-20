39f8d30d6073651d032e3b9e2f8d8484c5fa6172
user.email <developer3.codersh@gmail.com>
2025-11-20 10:43:13 +0530

    work page changes #

--- src/Components/Team/Team1.jsx
+++ src/Components/Team/Team1.jsx
@@ -6,83 +6,36 @@ const Team1 = () => {
     {
       img: '/assets/img/work/work1.webp',
       name: 'Reful Miya',
-      content: 'CO-Leader',
-      instagram: '#',
-      facebook: '#',
-      twitter: '#',
-      linkedin: '#',
     },
     {
       img: '/assets/img/work/work2.webp',
       name: 'Shikhon Islam',
-      content: 'Web Developer',
-      instagram: '#',
-      facebook: '#',
-      twitter: '#',
-      linkedin: '#',
     },
     {
       img: '/assets/img/work/work3.webp',
       name: 'Abdullah Islam',
-      content: 'Web Development',
-      instagram: '#',
-      facebook: '#',
-      twitter: '#',
-      linkedin: '#',
     },
     {
       img: '/assets/img/work/work1.webp',
       name: 'Leslie Alexander',
-      content: 'Nursing Assistant',
-      instagram: '#',
-      facebook: '#',
-      twitter: '#',
-      linkedin: '#',
     },
     {
       img: '/assets/img/work/work2.webp',
       name: 'Leslie Alexander',
-      content: 'Nursing Assistant',
-      instagram: '#',
-      facebook: '#',
-      twitter: '#',
-      linkedin: '#',
     },
     {
       img: '/assets/img/work/work3.webp',
       name: 'Leslie Alexander',
-      content: 'Nursing Assistant',
-      instagram: '#',
-      facebook: '#',
-      twitter: '#',
-      linkedin: '#',
     },
   ];
 
   return (
-    <>      <BreadCumb bgimg="/assets/img/breadcrumb.jpg" Title="Pricing"></BreadCumb>
-      <section className="team-section-3 fix section-padding">
+    <>
+      {/* WORK BREADCRUMB */}
+      <BreadCumb bgimg="/assets/img/breadcrumb.jpg" Title="Pricing"></BreadCumb>
 
-        {/* <div className="container px-3">
-          <div className="section-title-area">
-            <div className="section-title">
-              <div className="sub-title wow fadeInUp">
-                <span>OUR WORK</span>
-              </div>
-              <h2 className="wow fadeInUp" data-wow-delay=".3s">
-                Use SEO to Drive Growth <br /> at Your Business
-              </h2>
-            </div>
-            <div className="main-button wow fadeInUp" data-wow-delay=".5s">
-              <Link to="/team">
-                <span className="theme-btn">EXPLORE MORE </span>
-                <span className="arrow-btn">
-                  <i className="bi bi-arrow-up-right"></i>
-                </span>
-              </Link>
-            </div>
-          </div>
-        </div> */}
+      {/* WORK CARDS */}
+      <section className="team-section-3 fix section-padding">
         <div className="container-fluid">
           <div className="row">
             {teamContent.map((item, i) => (
