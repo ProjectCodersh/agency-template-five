35be39b8f5cb25765b276791f838afa360c632ae
user.email <developer3.codersh@gmail.com>
2025-11-24 18:40:27 +0530

    work page changed #

--- src/Components/Work/Work.jsx
+++ src/Components/Work/Work.jsx
@@ -4,22 +4,79 @@ import BreadCumb from '../../Components/Common/BreadCumb';
 const Work = () => {
     const teamContent = [
         {
-            img: '/assets/img/work/work1.webp',
+            img: '/assets/img/work/work1.png',
         },
         {
-            img: '/assets/img/work/work2.webp',
+            img: '/assets/img/work/work2.png',
         },
         {
-            img: '/assets/img/work/work3.webp',
+            img: '/assets/img/work/work3.png',
         },
         {
-            img: '/assets/img/work/work1.webp',
+            img: '/assets/img/work/work4.png',
         },
         {
-            img: '/assets/img/work/work2.webp',
+            img: '/assets/img/work/work5.png',
         },
         {
-            img: '/assets/img/work/work3.webp',
+            img: '/assets/img/work/work6.png',
+        },
+        {
+            img: '/assets/img/work/work7.png',
+        },
+        {
+            img: '/assets/img/work/work8.png',
+        },
+        {
+            img: '/assets/img/work/work9.png',
+        },
+        {
+            img: '/assets/img/work/work10.png',
+        },
+        {
+            img: '/assets/img/work/work11.png',
+        },
+        {
+            img: '/assets/img/work/work12.png',
+        },
+        {
+            img: '/assets/img/work/work13.png',
+        },
+        {
+            img: '/assets/img/work/work14.png',
+        },
+        {
+            img: '/assets/img/work/work15.png',
+        },
+        {
+            img: '/assets/img/work/work16.png',
+        },
+        {
+            img: '/assets/img/work/work17.png',
+        },
+        {
+            img: '/assets/img/work/work18.png',
+        },
+        {
+            img: '/assets/img/work/work19.png',
+        },
+        {
+            img: '/assets/img/work/work20.png',
+        },
+        {
+            img: '/assets/img/work/work21.png',
+        },
+        {
+            img: '/assets/img/work/work22.png',
+        },
+        {
+            img: '/assets/img/work/work23.png',
+        },
+        {
+            img: '/assets/img/work/work24.png',
+        },
+        {
+            img: '/assets/img/work/work25.png',
         },
     ];
 
@@ -28,6 +85,7 @@ const Work = () => {
             {/* BREADCRUMB */}
             <BreadCumb bgimg="/assets/img/breadcrumb.jpg" Title="Work" />
 
+            {/* Image section */}
             <section className="team-section-3 fix section-padding">
                 <div className="container-fluid">
                     <div className="row">
